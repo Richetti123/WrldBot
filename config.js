8f17250fc08bab14d62fa63547500cf7ff1ec891
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2023-12-25 23:33:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { en, es, id, ar, pt, fr, hi } from "./lib/idiomas/total-idiomas.js"
 global.owner = [
 ["51955095498", 'Richetti', true], //Richetti
 ["51933348947"], 
-["56945086055"], 
+["56966025876"], 
 ["56945086055"], 
 ["56945086055"],
 ["56945086055"],
@@ -103,31 +103,31 @@ global.moment = moment
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.official = [ // Agregate si eres Owner
-["56945086055", 'MichiBot', 1], 
-["56945086055", 'MichiBot', 1],  
-["56945086055", 'MichiBot', 1],
-["56945086055", 'MichiBot', 1],
-["56945086055", 'MichiBot', 1]]
+["56945086055", 'WrldBot', 1], 
+["56945086055", 'WrldBot', 1],  
+["56945086055", 'WrldBot', 1],
+["56945086055", 'WrldBot', 1],
+["56945086055", 'WrldBot', 1]]
 
 global.mail = '' //agrega tú correo
 global.desc = 'MichiBot el mejor bot de whatsapp' //agrega una descripción corta
 global.desc2 = 'MichiBot el mejor bot de whatsapp' //agrega una descripción larga (Solo se aplicará si su whasapp no tiene descripción)
 global.country = 'Perú' //agrega tú país ejemplo: 🇪🇨
 
-global.packname = "MichiBot"
-global.author = "MichiBot"
+global.packname = "WrldBot"
+global.author = "WrldBot"
 
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
 
-global.gt = "MichiBot"
-global.yt = "https://www.instagram.com/ceogeripium.dzn"
-global.yt2 = "https://www.instagram.com/ceogeripium.dzn"
-global.ig = "https://www.instagram.com/ceogeripium.dzn"
-global.md = "https://www.instagram.com/ceogeripium.dzn"
-global.fb = "https://www.instagram.com/ceogeripium.dzn"
+global.gt = "WrldBot"
+global.yt = "https://www.instagram.com/comunidad_wrld_ff"
+global.yt2 = "https://www.instagram.com/comunidad_wrld_ff"
+global.ig = "https://www.instagram.com/comunidad_wrld_ff"
+global.md = "https://www.instagram.com/comunidad_wrld_ff"
+global.fb = "https://www.instagram.com/comunidad_wrld_ff"
 
 global.nna = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //CANAL UPDATE
 global.nn2 = 'https://chat.whatsapp.com/IeKdVd3cPQt34jI7iqT4OR' //UPDATE
@@ -177,8 +177,8 @@ global.exito = eg
 
 //⊱ ━━━━━.⋅ Información | Information ⋅.━━━━ ⊰
 
-global.wm = "MichiBot"
-global.igfg = "MichiBot"
+global.wm = "WrldBot"
+global.igfg = "WrldBot"
 global.wait = "*⌛ _Cargando | Charging..._ ▬▭▭▭▭▭▭*"
 global.waitt = "*⌛ _Cargando | Charging..._ ▬▬▭▭▭*"
 global.waittt = "*⌛ _Cargando | Charging..._ ▬▬▬▬▭▭*"
